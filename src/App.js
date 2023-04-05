4375158f68f6f0fc2f7b8b190b3a8f2c04942f14
Dinh Truong <phamdinhtruong2511@gmail.com>
2023-04-05 09:40:24 +0700

    show, isshow

--- src/App.js
+++ src/App.js
@@ -1,119 +1,16 @@
 import './App.css';
-
+import Content from './Content'
 import {useState} from 'react'
-//#region Random Gift
-// const gifts = ['gift1', 'gift2', 'gift3', 'gift4', 'gift5']
-
-// function App() {
-
-//   const [gift, setGift] = useState()
-//   const randomGift = () => {
-//     const index = Math.floor(Math.random() * gifts.length)
-//     setGift(gifts[index]);
-//   }
-//   return (
-//     <div className="App" style={{padding: 20}}>
-//         <h1>{gift||'Chưa có phần thưởng'}</h1>
-//         <button onClick={randomGift}>Lấy thưởng</button>
-//     </div>
-//   );
-// } 
-//#endregion
-//#region input
-// function App(){
-//   const [name, setName] = useState('')
-//   const [email, setEmail] = useState('')
-//   console.log(name)
-//   const handleSubmit = () => {
-//     //call API
-//     console.log({name, email})
-//   }
-//   return (
-//     <div className="App" style={{padding:30}}>
-//       <input 
-//         value={name}
-//         onChange={(e) => setName(e.target.value)}
-//       />
-//       <input 
-//         value={email}
-//         onChange={(e) => setEmail(e.target.value)}
-//       />
-//       <button onClick={handleSubmit}>
-//         Register
-//       </button>
-//     </div>
-//   )
-// }
-//#endregion
-//#region Radio Button
-// const courses = [
-//   {
-//     id: 1,
-//     name: 'HTML'
-//   },
-//   {
-//     id: 2,
-//     name: 'CSS'
-//   },
-//   {
-//     id: 3,
-//     name: 'JavaScript'
-//   }
-// ]
-// function App(){
-//   const [checked, setChecked] = useState(1)
-//   console.log(checked)
-//   const handleSubmit = () => {
-//     //call API
-//     console.log({id: checked})
-//   }
-  
-//   return (
-//     <div className="App" style={{padding:30}}>
-//       {courses.map(course => (
-//         <div key={course.id}>
-//           <input type='radio'
-//           checked={course.id === checked}
-//             onChange={() => setChecked(course.id)}
-//           />{course.name}
-//         </div>
-//       ))}
-//       <button onClick={handleSubmit}>Submit</button>
-//     </div>
-//   )
-// }
-//#endregion
-//#region Checkbox
+// Mounted / Unmounted
 function App(){
-  const storageJobs = JSON.parse(localStorage.getItem('jobs'))
-
-  const [job, setJob] = useState('')
-  const [jobs, setJobs] = useState(storageJobs ?? [])
-  const handleSubmit = () => {
-    setJobs(prev => {
-      const newJobs = [...prev, job]
-      // Save to localStorage
-      const jsonJobs = JSON.stringify(newJobs)
-      localStorage.setItem('jobs', jsonJobs)
-      return newJobs
-    })
-    setJob('')
-  }
+  const [showContent, setShowContent] = useState(false)
   return (
     <div className="App" style={{padding:30}}>
-      <input 
-        value={job}
-        onChange={e => setJob(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Submit</button>
-      <ul>
-         {console.log(jobs)}
-          {jobs.map((job,index) => (
-            <li key={index}>
-              {job}
-            </li>
-          ))}
-      </ul>
+      <button onClick={() => 
+        setShowContent(!showContent)}>
+          Show Content
+      </button>
+      {showContent && <Content/>}
     </div>
   )}
 //#endregion
---
+++ src/Content.js
@@ -0,0 +1,9 @@
+function Content(){
+    return(
+        <div style={{padding : 32}}>
+            <h1>Hi anh em</h1>
+        </div>
+    )
+}
+
+export default Content
