6cc667c03855edcabffa57a4ccf542cfd6ea8d38
Dinh Truong <phamdinhtruong2511@gmail.com>
2023-04-06 12:30:49 +0700

    hooks/useReducer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import './App.css';
 import Content from './Content'
-import {useState} from 'react'
-// Mounted / Unmounted
+import {useReducer} from 'react'
+
+//init state
+const initState = 0
+// Actions
+const UP_ACTION = 'up'
+const DOWN_ACTION = 'down'
+// Reducer
+const reducer = (state, action) => {
+  console.log('Reducer-run...') 
+  switch (action) {
+    case UP_ACTION:
+      return state + 1
+    case DOWN_ACTION:
+      return state - 1
+    default: 
+      throw new Error('Invalid action')
+  }
+}
 function App(){
-  const [showContent, setShowContent] = useState(false)
-  return (
-    <div className="App" style={{padding:30}}>
-      <button onClick={() => 
-        setShowContent(!showContent)}>
-          Show Content
-      </button>
-      {showContent && <Content/>}
-    </div>
-  )}
+    const [count, dispatch] = useReducer(
+      reducer, initState
+    )
+
+    return (
+      <div>
+        <h1>{count}</h1>
+        <button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
+        <button onClick={() => dispatch(UP_ACTION)}>Up</button>
+      </div>
+    )
+  }
 //#endregion
 export default App;
