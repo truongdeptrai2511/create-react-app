2eef8715af47af921646331c2c61ecf6a5913439
Dinh Truong <phamdinhtruong2511@gmail.com>
2023-04-06 10:41:43 +0700

    hooks/memo

--- src/Content.js
+++ src/Content.js
@@ -1,58 +1,18 @@
-import { useEffect, useState, useLayoutEffect, useRef} from "react"
-// useEffect được sử dụng khi cần Side effects
-// Cần nắm vững: 
-// Events: Add / remove  event listeners
-// Observer partern: Subcribe / unsubscribe
-// Closures: Function that returns a function
-// Timers: setInterval, clearInterval, setTimeout, clearTimeout
-// useState
-// mounted / unmounted
-// ===
-// Call API
+import React, { useEffect, useState, useLayoutEffect, useRef, memo} from "react"
+// 1. memo() -> higher order component  (hoc)
+// 2. useCallBack()
 
-/**
- * 
- * 1. Update DOM
- *  - F8 Blog title
- * 2. Call API
- * 3. Listen DOM events
- *  - Scroll 
- *  - Resize
- * 4. Cleanup
- *  - Remove listener / Unsubscribe
- *  - Clear timer
- */
-
+//Hooks
+//HOC
+// Render props
 
 function Content(){
-    const [count, setCount] = useState(60)
-
-    let timerId = useRef()
-    const prevCount = useRef()
-
-    useEffect(() => {
-        prevCount.current = count
-    }, [count])
-
-    const handleStart = () => {
-        timerId.current = setInterval(() => {
-            setCount(prevCount => prevCount - 1)
-        }, 1000)
-        console.log('start -> ', timerId.current)
-    }
-    const handleStop = () => {
-        clearInterval(timerId.current)
-
-        console.log('stop -> ', timerId.current)
-    }
     
     return (
         <div>
-            <button onClick={handleStart}>Start</button>
-            <button onClick={handleStop}>Stop</button>
-            <h1>{count}</h1>
+            <h1>Hello Content!</h1>
         </div>
     )
 }
 
-export default Content
+export default memo(Content)
